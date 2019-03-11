605bb18e94b79a9286fa9c77bd8c7bc3f4e8cee7
mmoanis <mohamed_moanis@outlook.com>
2019-03-11 15:22:31 +0100

    Update ThreadPool class
    Rewrite the internal representation of the ThreadPool class. The class will store simple functions in the work queue that wraps a heap allocated packed_task which will execute the required function. This will allow the ThreadPool to return a std::future that has a valid state and can be used to retrieve the task result once it is ready.

--- src/threadPool.cpp
+++ src/threadPool.cpp
@@ -4,40 +4,45 @@
 ThreadPool::ThreadPool(size_t number_of_workers)
 {
     auto worker = [this]() {
-        while (!finished_ || task_queue_.size() > 0) {
-            Task task;
-            if (popFromTaskQueue(task) && task.valid()) {
-                task();
+        while (true) {
+            InternalTaskType task;
+
+            // try to get a task to execute from the shared work queue.
+            // this section is considered critical as race conditions
+            // can happen due to the fact that the underlying queue is
+            // not thread safe.
+            {
+                // lock the critical section since we are going to do
+                // some operations on the shared queue
+                std::unique_lock<std::mutex> lock(mutex_);
+
+                // wait until there are events in the task queue to consume
+                // or otherwise that we got a signal that there are no more tasks
+                // that will be submitted in the future to wait for
+                condition_.wait(lock, [this](){
+                    return !task_queue_.empty() || finished_;
+                });
+
+                // we need to check after waking up if the wake up signal means
+                // that there are no more work to be done
+                if (finished_ && task_queue_.empty()) {
+                    break;
+                }
+
+                // get the task out of the queue  
+                task = std::move(task_queue_.front());
+                task_queue_.pop();
             }
+            
+            // execute the task
+            task();
         }
     };
 
+    // construct the worker threads
     for (size_t i = 0; i < number_of_workers; ++i) {
-        workers_.push_back(std::thread(worker));
+        workers_.push_back(std::thread(std::move(worker)));
     }
-}
-
-// Helper method to safely pop an event from the shared task queue.
-bool ThreadPool::popFromTaskQueue(Task &t)
-{
-    std::unique_lock<std::mutex> lock(mutex_);
-
-    // wait until there are events in the task queue to consume
-    // or otherwise that we got a signal that there are no more tasks
-    // that will be submitted in the future to wait for
-    condition_.wait(lock, [this](){
-        return task_queue_.size() > 0 || finished_;
-    });
-
-    if (task_queue_.size() == 0) {
-        return false;
-    }
-
-    // get the firt read event
-    t = std::move(task_queue_.front());
-    task_queue_.pop();
-
-    return true;
 }
 
 // Submits an event to be executed by the workers threads.
@@ -46,40 +51,42 @@ bool ThreadPool::popFromTaskQueue(Task &t)
 // then the events will be executed in the client's thread.
 // Otherwise this will just enqueue the task and let the workers finish
 // the job.
-ThreadPool::TaskResult ThreadPool::submit(const Event& e)
+ThreadPool::TaskResult ThreadPool::submit(ThreadPool::TaskType&& func,
+    ThreadPool::TaskParamType&& param)
 {
-    // create the task function that will be executed
-    Task t([e, this](){
-        std::lock_guard<std::mutex> lock(mutex_io_);
-        std::cout << "Worker #" << std::this_thread::get_id() << " executed event #" << e.getNumber() << '\n';
+    TaskResult result;
 
-        return "";
-    });
-
+    // in case there are no worker threads, we are going to directly execute
+    // the task on the caller thread since we have none.
     if (workers_.size() > 0) {
-        // add the task to the queue
-        submitToTaskQueue(std::move(t));
+        // allocate task to be executed
+        auto task = std::make_shared<std::packaged_task<std::string()>> (
+            std::bind(std::forward<TaskType>(func), std::forward<TaskParamType>(param)));
+        result = task->get_future();
+
+        // insert the task in the work queue
+        // this is a critical section since we are modifying a shared resource
+        {
+            // lock the work queue mutex
+            std::lock_guard<std::mutex> lock(mutex_);
+
+            // insert the event in the queue wrapped by a simple function
+            // with no return type
+            task_queue_.emplace([task](){(*task)();});
+        }
+
+        // notify one of the workers waiting for tasks
+        condition_.notify_one();
     } else {
         // execute on caller thread since we have no workers
-        t();
+        // no need for any heap allocations
+        std::packaged_task<std::string(std::shared_ptr<Event>)> task(func);
+        result = task.get_future();
+
+        task(param);
     }
 
-    return t.get_future();
-}
-
-// Helper method to safely push an event to the shared task queue.
-void ThreadPool::submitToTaskQueue(Task && t)
-{
-    // enter the critical section
-    {
-        std::lock_guard<std::mutex> lock(mutex_);
-
-        // insert the event in the queue
-        task_queue_.push(std::move(t));
-    }
-
-    // notify workers waiting for tasks
-    condition_.notify_one();
+    return result;
 }
 
 // Block waiting for the execution of all submitted events.
@@ -87,7 +94,13 @@ void ThreadPool::submitToTaskQueue(Task && t)
 void ThreadPool::execute()
 {
     // signal that task submission is done
-    finished_.store(true);
+    {
+        // lock the critical section using the same mutex
+        // since we are going to modify a shared variable
+        // that other threads are waiting on
+        std::unique_lock<std::mutex> lock(mutex_);
+        finished_ = true;
+    }
 
     // notify all workers about the changes
     condition_.notify_all();
--- src/threadPool.hpp
+++ src/threadPool.hpp
@@ -13,48 +13,51 @@
 // Each event is considered an individal task that are submitted to the
 // pool and executed by one of the pool's worker threads. The number of
 // worker threads is specified by client of the class.
+//
 // Implements a typical producer consumer pattern but with only 1
 // producer and one or more consumers.
 class ThreadPool
 {
 public:
-    using Task = std::packaged_task<std::string(void)>;
     using TaskResult = std::future<std::string>;
+    using TaskParamType = std::shared_ptr<Event>;
+    using TaskType = std::function<std::string(TaskParamType)>;
 
     // Constructor by default assumes the execution is on one thread.
     explicit ThreadPool(size_t number_of_workers = 8);
 
+    // Copys are not allowed.
     ThreadPool(const ThreadPool&) = delete;
     ThreadPool& operator=(const ThreadPool&) = delete;
 
     // Submits an event to be executed by the workers threads.
+    // The parameter fun is the function that executes the simulation
+    // for an event which is passed to it as a parameter.
     //
     // If there are no workers -ie: the number of workers was set to be zero-
     // then the events will be executed in the client's thread.
     // Otherwise this will just enqueue the task and let the workers finish
     // the job.
-    TaskResult submit(const Event& e);
+    TaskResult submit(TaskType&& func, TaskParamType&& param);
 
     // Block waiting for the execution of all submitted events.
     // Any tasks submitted after this call will not be executed.
     void execute();
 
 private:
-    // Helper method to safely push an event to the shared task queue.
-    void submitToTaskQueue(Task && t);
-
-    // Helper method to safely pop an event from the shared task queue.
-    bool popFromTaskQueue(Task &t);
+    // Internal storage type of task
+    // tasks are wrapped around into a function with no return type or args.
+    using InternalTaskType = std::function<void(void)>;
 
     // conditional variable for managing the shared queue
     std::condition_variable condition_;
 
     // internal state of the executer. This flag is set when execute method is
     // called to signal the workers to finish the tasks they have.
-    std::atomic_bool finished_ {false};
+    bool finished_ {false};
 
     // task queue
-    std::queue<std::packaged_task<std::string(void)>> task_queue_;
+    std::queue<InternalTaskType> task_queue_;
 
     // mutex for making thread safe operations on the task queue.
     std::mutex mutex_;
