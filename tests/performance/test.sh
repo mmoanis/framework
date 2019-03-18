170e3e3ae1f69ea70c70925f2d5a0151d5b08a19
mmoanis <mohamed_moanis@outlook.com>
2019-03-18 22:14:07 +0100

    Update performance test script

--- tests/performance/test.sh
+++ tests/performance/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for event in 100 1000 10000 100000 1000000 10000000; do
-	for cpu in 1 2 4 6 8; do
+	for cpu in 1 ; do
 		echo "#events=$event, #cpu=$cpu" | tee -a run.log
 		
 		# generate test file configuration
 		echo "number_of_events = $event" > sample.conf
-		if [ "$event" -ne "1" ]; then
+		if [ "$cpu" -ne "1" ]; then
 			echo "number_of_threads = $cpu" >> sample.conf
 		fi
 		echo "modules = Module1 Module2 Module3" >> sample.conf
